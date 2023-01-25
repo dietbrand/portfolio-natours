cb8bd3031b46f22c6c63e0b43e5c72e1719fd5b3
dietbrand <dietbrand.tollenaere@gmail.com>
2023-01-25 14:43:29 +0100

    Add readme

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,7 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
 const Booking = require('../models/bookingModel');
-const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
---
+++ controllers/readme.md
@@ -0,0 +1,5 @@
+# Natours App
+
+This is a node.js application I built using a [Udemy course](https://www.udemy.com/course/nodejs-express-mongodb-bootcamp).
+
+It uses express, mongoose, mongodb, pug and heavily features the MVC design strategy.
