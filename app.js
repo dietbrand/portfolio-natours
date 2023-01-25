7d63f87eb32462ccba9ca775b49f62b1d856bbda
dietbrand <dietbrand.tollenaere@gmail.com>
2023-01-25 15:48:24 +0100

    Added CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,8 +27,14 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // Global Middleware
+// Implement CORS
+app.use(cors());
+
+app.options('*', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
+
 // Security HTTP headers
 const scriptSrcUrls = [
   'https://unpkg.com/',
